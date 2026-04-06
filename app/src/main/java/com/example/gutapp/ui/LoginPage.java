9e4e64443029ddaae2759eaa078e5b147cbb969e
darkigg <97245358+darkigg@users.noreply.github.com>
2026-04-06 18:51:35 +0300

    improved app logo and splash screen

--- app/src/main/java/com/example/gutapp/ui/LoginPage.java
+++ app/src/main/java/com/example/gutapp/ui/LoginPage.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -36,6 +37,7 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
